b83bd8ee3134a101d747499628bc85065ce5d821
ZuxNbg <zux85@yahoo.com>
2014-05-24 13:38:45 +0200

    First app
    This is first app make in devxpress!

---
+++ views/NewView.js
@@ -0,0 +1,10 @@
+﻿Application1.NewView = function (params) {
+
+    var viewModel = {
+        //  Put the binding properties here
+
+        message: ko.observable('Hello ' + params.id + '!')
+    };
+
+    return viewModel;
+};
---
+++ views/home.js
@@ -0,0 +1,15 @@
+﻿Application1.home = function () {
+        var viewModel = {
+                message: ko.observable('Welcome!'),
+            name: ko.observable(''),
+           kaziCao: function () {
+               this.message("Hello " + this.name() + '!');
+           },
+               greet: function()
+               {
+                   Application1.app.navigate("NewView/" + this.name());
+               }
+               
+      };
+    return viewModel;
+};
